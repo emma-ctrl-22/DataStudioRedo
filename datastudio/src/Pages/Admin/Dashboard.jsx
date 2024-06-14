0a4e37ab4bf8344d3f32ad32168a353e68e24bad
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2024-06-14 21:20:49 +0000

    prime react package installed

--- datastudio/src/Pages/Admin/Dashboard.jsx
+++ datastudio/src/Pages/Admin/Dashboard.jsx
@@ -4,6 +4,11 @@ import Card from "./components/Card"; // Adjust the import path if necessary
 const Dashboard = () => {
   return (
     <div className="w-full h-full mx-0 px-0 py-0 bg-gray-100">
+      <div className="mt-5 mx-3 rounded-md">
+        <h1 className="text-md text-left text-red-500 ">
+          <span className="bg-red-100 p-1 rounded-md">Dashboard</span>
+        </h1>
+      </div>
       <div className="grid grid-cols-3 gap-4 p-4">
         <Card title="Total Users" value="150" />
         <Card title="Total Reports" value="100" />
